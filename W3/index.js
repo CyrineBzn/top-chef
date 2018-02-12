36e889924d762b6c123755f7d7b5604606bd3df4
Cyrine Bouzayene <cyrinebouz@hotmail.fr>
2018-02-12 21:54:14 +0100

    Function to get the total number of pages

--- W3/index.js
+++ W3/index.js
@@ -3,23 +3,24 @@ var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-
+/*
 app.get('/scrape', function(req, res){
   // Let's scrape Anchorman 2
   url = 'https://restaurant.michelin.fr/28u6ql7/le-jardin-des-remparts-beaune'
   request(url, function(error, response, html){
     if(!error){
       var $ = cheerio.load(html);
-      var Name;
-      var json = { Name : ""};
+      var Name, Location;
+      var json = { Name : "", Location: ""};
 
       $('.poi_intro-display-title').filter(function(){
         var data = $(this);
-        Name = data.text().trim();
+        Name = data.text().trim(); 
         json.Name = Name;
        
       })
     }
+    
     fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
       console.log('File successfully written! - Check your project directory for the output.json file');
     })
@@ -29,4 +30,19 @@ app.get('/scrape', function(req, res){
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
-exports = module.exports = app;
+exports = module.exports = app;*/
+
+function get_number_page(url, callback) {
+  request(url, function (error, response, html) {
+       if (!error) {
+           var $ = cheerio.load(html);
+           var nbr = $('ul.pager').children('.last').prev().children().html();
+           callback(nbr);
+       }
+   });
+ }
+
+
+ 
+ get_number_page('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin',console.log);
+   
