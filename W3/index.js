ca1246409757811d87233003adcaad6ab805fcab
Cyrine Bouzayene <cyrinebouz@hotmail.fr>
2018-02-14 12:42:12 +0100

    Scrapping done and writing a json file

--- W3/index.js
+++ W3/index.js
@@ -5,7 +5,6 @@ var cheerio = require('cheerio');
 var app     = express();
 /*
 app.get('/scrape', function(req, res){
-  // Let's scrape Anchorman 2
   url = 'https://restaurant.michelin.fr/28u6ql7/le-jardin-des-remparts-beaune'
   request(url, function(error, response, html){
     if(!error){
@@ -45,7 +44,7 @@ function get_number_page(url, callback) {
       var urls_tab = [];
       request(url, function (error, response, html) {
           if (!error) {
-              const $ = cheerio.load(html);
+              var $ = cheerio.load(html);
               $('a[class=poi-card-link]').each(function (i, elem) {
                 urls='https://restaurant.michelin.fr' + $(elem).attr('href')
                 urls_tab.push(urls);
@@ -56,8 +55,54 @@ function get_number_page(url, callback) {
       });
   }
   
- 
- get_number_page('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin',console.log);
- get_url_page('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin');
+function get_infos_restaurent(url, callback){
+  request(url, function (error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+      var name,street,city,postalcode,locality,country,chef,price ;
+      var name = $('h1').first().text();
+      var street= $('.thoroughfare').first().text();
+      var postalcode = $('.postal-code').first().text();;
+      var locality = $('.locality').first().text();
+      var country= $('.country').first().text();
+      var chef = $('.field--name-field-chef').children('.field__items').text();
+      var price= $('.poi_intro-display-prices').first().text().trim();
+      var restaurant = {
+              "name": name,
+              "street": street,
+              "postalcode": postalcode,
+              "locality": locality,
+              "country": country,
+              "address": street + ' ,' + postalcode + ' ' + locality+' ,'+ country,
+              "chef": chef,
+              "price": price,
+              "url": url
+                    };
+  if(callback) callback(restaurant); 
+  console.log(restaurant);
+     }
+  });
+}
 
-   
+function write_json(url){
+  var json = { "restaurants": [] };
+  get_number_page(url, function (nbr) {
+  for (var i = 1; i < +nbr + 1; i++) {
+    get_url_page(url + '/page-' + i.toString(), function (arr) {
+      arr.forEach(function (elem) {
+       get_infos_restaurent(elem, function (restaurant) {
+        json.restaurants.push(restaurant);
+         fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+           console.log('Restaurent successfully written! - Check your project directory for the output.json file');
+         });
+       });
+      });
+    });
+   }
+  });
+}
+
+ //get_number_page('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin');
+ //get_url_page('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin');
+ //get_infos_restaurent('https://restaurant.michelin.fr/28u6ql7/le-jardin-des-remparts-beaune');
+ write_json('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin');
